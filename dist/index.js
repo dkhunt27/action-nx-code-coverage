6406530816b03f6394fba0c7a79d6dad177be184
Dan Hunt <dkhunt27@gmail.com>
2021-12-16 12:03:30 -0500

    updated logger to use console

--- dist/index.js
+++ dist/index.js
@@ -728,31 +728,33 @@ const log = (level, name,
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 value) => {
     const debug = (0, core_1.getInput)('debug') === 'true' || false;
-    if (debug) {
-        if (level === 'warn') {
-            // eslint-disable-next-line no-console
-            console.log(`WARNING: ${name}`, value);
-        }
-        if (level === 'info') {
-            // eslint-disable-next-line no-console
-            console.log(name, value);
-        }
-        if (level === 'debug') {
+    // if (debug) {
+    if (level === 'warn') {
+        // eslint-disable-next-line no-console
+        console.log(`WARNING: ${name}`, value);
+    }
+    if (level === 'info') {
+        // eslint-disable-next-line no-console
+        console.log(name, value);
+    }
+    if (level === 'debug') {
+        if (debug) {
             // eslint-disable-next-line no-console
             console.log(`DEBUG: ${name}`, value);
         }
     }
-    else {
-        if (level === 'warn') {
-            (0, core_1.setOutput)(`WARNING: ${name}`, value);
-        }
-        if (level === 'info') {
-            (0, core_1.setOutput)(name, value);
-        }
-        if (level === 'debug') {
-            (0, core_1.setOutput)(`DEBUG: ${name}`, value);
-        }
-    }
+    // setOutput didn't seem like it was working
+    // } else {
+    //   if (level === 'warn') {
+    //     setOutput(`WARNING: ${name}`, value)
+    //   }
+    //   if (level === 'info') {
+    //     setOutput(name, value)
+    //   }
+    //   if (level === 'debug') {
+    //     setOutput(`DEBUG: ${name}`, value)
+    //   }
+    // }
     return;
 };
 exports.log = log;
--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import {getInput, setOutput} from '@actions/core'
+import {getInput} from '@actions/core'
 
 export const log = (
   level: 'warn' | 'info' | 'debug',
@@ -8,34 +8,37 @@ export const log = (
 ): void => {
   const debug = getInput('debug') === 'true' || false
 
-  if (debug) {
-    if (level === 'warn') {
-      // eslint-disable-next-line no-console
-      console.log(`WARNING: ${name}`, value)
-    }
+  // if (debug) {
+  if (level === 'warn') {
+    // eslint-disable-next-line no-console
+    console.log(`WARNING: ${name}`, value)
+  }
 
-    if (level === 'info') {
-      // eslint-disable-next-line no-console
-      console.log(name, value)
-    }
+  if (level === 'info') {
+    // eslint-disable-next-line no-console
+    console.log(name, value)
+  }
 
-    if (level === 'debug') {
+  if (level === 'debug') {
+    if (debug) {
       // eslint-disable-next-line no-console
       console.log(`DEBUG: ${name}`, value)
     }
-  } else {
-    if (level === 'warn') {
-      setOutput(`WARNING: ${name}`, value)
-    }
-
-    if (level === 'info') {
-      setOutput(name, value)
-    }
-
-    if (level === 'debug') {
-      setOutput(`DEBUG: ${name}`, value)
-    }
   }
+  // setOutput didn't seem like it was working
+  // } else {
+  //   if (level === 'warn') {
+  //     setOutput(`WARNING: ${name}`, value)
+  //   }
+
+  //   if (level === 'info') {
+  //     setOutput(name, value)
+  //   }
+
+  //   if (level === 'debug') {
+  //     setOutput(`DEBUG: ${name}`, value)
+  //   }
+  // }
 
   return
 }
