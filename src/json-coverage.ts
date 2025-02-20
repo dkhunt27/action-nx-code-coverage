9ec48e497d9270dfe764ae81ecd882985e764983
Timo Ströhlein <39571859+timostroehlein@users.noreply.github.com>
2025-02-20 13:43:17 +0100

    add debug logs

--- src/json-coverage.ts
+++ src/json-coverage.ts
@@ -12,7 +12,7 @@ import {
   JcsParsedType,
   SummaryFileListType
 } from './types'
-import {debug as logDebug, warning as logWarn} from '@actions/core'
+import {debug as logDebug, warning as logWarn, info as logInfo} from '@actions/core'
 import {readFileSync, readdirSync, statSync} from 'fs'
 import {TextReport} from './istanbul-reports-text'
 import libCoverage from 'istanbul-lib-coverage'
@@ -64,6 +64,8 @@ export const mergeFileLists = ({
       summary.parsedTotal.statements.pct.toString() === 'Unknown'
         ? 0
         : summary.parsedTotal.statements.pct
+    logInfo(`summaryCoveragePct ${summaryCoveragePct}`)
+    logInfo(`summary ${JSON.stringify(summary, null, 2)}`)
 
     const final = finalFileList.find(item => item.app === summary.app)
     const finalParsed = final ? final.parsed : ''
@@ -76,9 +78,13 @@ export const mergeFileLists = ({
     const found = baseSummaryFileList.find(item => item.app === summary.app)
     if (found) {
       base = buildMergeItem(found)
-      baseCoveragePct = base.parsedTotal.statements.pct.toString() === 'Unknown'
-        ? 0
-        : base.parsedTotal.statements.pct
+      logInfo(`base ${JSON.stringify(base, null, 2)}`)
+      baseCoveragePct =
+        base.parsedTotal.statements.pct.toString() === 'Unknown'
+          ? 0
+          : base.parsedTotal.statements.pct
+      logInfo(`baseCoveragePct ${baseCoveragePct}`)
+      logInfo(`summaryCoveragePct ${summaryCoveragePct}`)
 
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/EPSILON
       // use Number.EPSILON so rounding of 0.0005 is correct
@@ -86,6 +92,7 @@ export const mergeFileLists = ({
         Math.round(
           (summaryCoveragePct - baseCoveragePct + Number.EPSILON) * 100
         ) / 100
+      logInfo(`diff ${diff}`)
     } else {
       // if not in base, assuming it is all new coverage so set base to 0
       baseCoveragePct = 0
